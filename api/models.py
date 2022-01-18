100193cce885a7929d455b6cf4c0a568cab4a5d7
inomoto daiya <daiandaian2525@gmail.com>
2022-01-18 18:39:24 +0900

    modelsファイル修正

--- api/models.py
+++ api/models.py
@@ -2,7 +2,7 @@ from django.db import models;
 
 class News(models.Model):
     title = models.CharField(max_length=50)
-    body = models.CharField(max_length=500)
+    body = models.CharField(max_length=10000)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- api/serializers.py
+++ api/serializers.py
@@ -3,8 +3,8 @@ from .models import News
 
 class NewsSerializer(serializers.ModelSerializer):
 
-    created_at = serializers.DateTimeField(format="%Y-%m-%d", read_only=True)
-    updated_at = serializers.DateTimeField(format="%Y-%m-%d", read_only=True)
+    created_at = serializers.DateTimeField(format="%Y年%m月%d日", read_only=True)
+    updated_at = serializers.DateTimeField(format="%Y年%m月%d日", read_only=True)
 
 
     class Meta:
